f88627a24b6c4730ebd3b25c5cb5c6492c658cab
Thomas Boos Pegler <tboospegler@gmail.com>
2022-07-20 14:55:54 -0400

    Requisito 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetImage, planetName } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{ planetName }</p>
+      </div>
+    );
+  }
+}
+
+export default PlanetCard;
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <PlanetCard planetImage="" planetName="Teste" />
       </div>
     );
   }
